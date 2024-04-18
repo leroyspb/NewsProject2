4079312191e559c89c49d296a140b570c37ede3e
leroyspb <leroy777@bk.ru>
2024-04-18 07:06:19 +0300

    addedNewsAndArticlesFromD1

--- news_project/news/views.py
+++ news_project/news/views.py
@@ -2,26 +2,37 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView, DetailView
-from .models import New
+from .models import Post
+from datetime import datetime
 
 
-class NewsList(ListView):
-    # Указываем модель, объекты которой мы будем выводить
-    model = New
-    # Поле, которое будет использоваться для сортировки объектов
-    ordering = 'name'
-    # Указываем имя шаблона, в котором будут все инструкции о том,
-    # как именно пользователю должны быть показаны наши объекты
+class PostList(ListView):
+    model = Post
     template_name = 'news.html'
-    # Это имя списка, в котором будут лежать все объекты.
-    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
-    context_object_name = 'news'
+    context_object_name = 'posts'
+    ordering = '-creation_time_in'
+
+# Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # context['time_creation'] = datetime.astimezone()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_sale'] = "Распродажа в среду!"
+        return context
 
 
-class NewDetail(DetailView):
+class PostDetail(DetailView):
     # Модель всё та же, но мы хотим получать информацию по отдельному товару
-    model = New
-    # Используем другой шаблон — new.html
-    template_name = 'new.html'
+    model = Post
+    # Используем другой шаблон — post_detail.html
+    template_name = 'post.html'
     # Название объекта, в котором будет выбранный пользователем продукт
-    context_object_name = 'new'
+    # context_object_name = 'post'
--- news_project/news_project/urls.py
+++ news_project/news_project/urls.py
@@ -19,9 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pages/', include('django.contrib.flatpages.urls')),
-    # Делаем так, чтобы все адреса из нашего приложения (simpleapp/urls.py)
-   # подключались к главному приложению с префиксом news/.
+    path('pages/', include('django.contrib.flatpages.urls')),  # приложение flatpages
+    # подключались к главному приложению с префиксом news/.
     path('news/', include('news.urls')),
     # path('news/', include('news_project.urls')),
 ]
