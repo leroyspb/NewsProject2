6a4a4d11d267027d4d5fd6fc9f6e366d71914d6b
leroyspb <leroy777@bk.ru>
2024-06-14 07:10:41 +0300

    _14_06_admin.py

--- news_project/news/admin.py
+++ news_project/news/admin.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from .models import *
 
 
-class PostAdmin(admin.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin):
+    # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    list_display = ('title', 'author', 'categoryType', 'rating') # оставляем только имя и цену товара
+    list_filter = ('rating', 'author', 'title') # добавляем примитивные фильтры в нашу админку
     fields = ['author', 'title', 'text', 'categoryType']
-    list_display = ('title', 'author', 'categoryType', 'rating')
+    search_fields = ('title', 'categoryType') # тут всё очень похоже на фильтры из запросов в базу
 
 
-admin.site.register(Post, PostAdmin)
+admin.site.register(Post, ProductAdmin)
 admin.site.register(Category)
 admin.site.register(PostCategory)
 admin.site.register(Author)
